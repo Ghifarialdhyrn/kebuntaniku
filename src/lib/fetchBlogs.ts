04d06b4a2b9b4e4261f725bc152b91d5227ab86e
gifarialdhynr@gmail.com <gifarialdhynr@gmail.com>
2026-01-22 11:00:19 +0700

    fix code

--- src/lib/fetchBlogs.ts
+++ src/lib/fetchBlogs.ts
@@ -2,7 +2,8 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN!, // 🔥
+  host: "preview.contentful.com", // 🔥 bypass CDN
 });
 
 export async function fetchBlogs() {
--- src/lib/fetchProducts.ts
+++ src/lib/fetchProducts.ts
@@ -2,13 +2,14 @@ import { createClient } from "contentful";
 
 const client = createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN!,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_PREVIEW_TOKEN!, // 🔥
+  host: "preview.contentful.com",
 });
 
 export async function fetchProducts() {
   const res = await client.getEntries({
     content_type: "productsKebunTaniku",
-    order: ["-sys.createdAt"], // 🔥 produk terbaru di atas
+    order: ["-sys.createdAt"],
   });
 
   return res.items;
